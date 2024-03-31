4e282e8d4b898528ef9d5bcd914581ac821cffaa
nsavvide <niels.i.savvides@gmail.com>
2024-03-31 20:44:02 +0200

    :sparkles: Update app with new pages

--- portfolio-niels/src/App.jsx
+++ portfolio-niels/src/App.jsx
@@ -1,6 +1,21 @@
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Experience } from "./pages/Experience";
+import { Home } from "./pages/home";
+import { Projects } from "./pages/Projects";
+
 function App() {
   return (
-    <div>
+    <div className="App">
+      <Navbar>
+        <Home />
+        <Projects />
+        <Experience />
+        <About />
+        <Contact />
+      </Navbar>
     </div>
   );
 }
