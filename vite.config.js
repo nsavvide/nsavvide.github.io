61946d7966956be4e00234d3e20c6729c14b68ca
nsavvide <niels.i.savvides@gmail.com>
2024-04-11 15:53:53 +0200

    :wrench: Change base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nsavvide.github.io/",
-  plugins: [react()],
-  assetsInclude: ["**/*.glb"],
+    base: "/nsavvide.github.io",
+    plugins: [react()],
+    assetsInclude: ["**/*.glb"],
 });
